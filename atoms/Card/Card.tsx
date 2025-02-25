f544cebe371641e185017b4b573d7a2240b6e504
Marcela Malaver <marcela9409@gmail.com>
2025-02-25 15:57:55 -0500

    styles helper and hoc

--- atoms/Card/Card.tsx
+++ atoms/Card/Card.tsx
@@ -1,32 +1,26 @@
 import React from "react";
-import classNames from "classnames";
 import styles from "./Card.module.css";
 import { CardProps } from "./types";
+import withStyles from "../../hocs/withStyles";
 
 export const Card = ({
+  getStyles,
   onClick,
   isClickable,
   isDraggable,
   children,
-  color = 'base',
-  size = 'sm',
 }: CardProps) => {
   return (
     <div
       onClick={onClick}
-      className={classNames(
-        styles.card,
-        styles[`color-${color}`],
-        styles[`size-${size}`],
-        {
-          [styles["is-clickable"]]: isClickable,
-          [styles["is-draggable"]]: isDraggable,
-        }
-      )}
+      className={getStyles("card", ["color", "size"], {
+        "is-clickable": isClickable,
+        "is-draggable": isDraggable,
+      })}
     >
       {children}
     </div>
   );
 };
 
-export default Card;
+export default withStyles(styles)(Card);
